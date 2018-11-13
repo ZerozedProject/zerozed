8c6a936ccd3c49eed7241521d46bea98109b9f44
Peter Bushnell <bushsolo@gmail.com>
2018-11-13 09:59:14 +0000

    URI

--- src/qt/locale/bitcoin_lv_LV.ts
+++ src/qt/locale/bitcoin_lv_LV.ts
@@ -218,11 +218,11 @@
         <translation>Ciļņu rīkjosla</translation>
     </message>
     <message>
-        <source>Request payments (generates QR codes and bitcoin: URIs)</source>
-        <translation>Pieprasīt maksājumus (izveido QR kodu un bitcoin: URIs)</translation>
+        <source>Request payments (generates QR codes and zerozed: URIs)</source>
+        <translation>Pieprasīt maksājumus (izveido QR kodu un zerozed: URIs)</translation>
     </message>
     <message>
-        <source>Open a bitcoin: URI or payment request</source>
+        <source>Open a zerozed: URI or payment request</source>
         <translation>Atvērt bitcoin URI vai maksājuma pieprasījumu</translation>
     </message>
     <message>
